c0b1ce934158da24fd981fe63a403ae4069c9e9e
te-dious <tanmay@qoala.id>
2023-04-05 01:30:13 +0530

    set env variable

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
-    apiUrl: 'http://localhost:8000/splitwise/auth/',
-    userApiUrl: 'http://localhost:8000/splitwise/user_info/',
-    logoutUrl: 'http://localhost:8000/splitwise/logout/',
-    groupApiUrl: 'http://localhost:8000/splitwise/groups_info/',
-    friendsApiUrl: 'http://localhost:8000/splitwise/friends_info/',
-    uploadApiUrl: 'http://localhost:8000/splitwise/upload_csv/',
-    createBulkTransactions: 'http://localhost:8000/splitwise/create_bulk_transactions/'
-  };
+  apiUrl: `${process.env.REACT_APP_API_HOST}/splitwise/auth/`,
+  userApiUrl: `${process.env.REACT_APP_API_HOST}/splitwise/user_info/`,
+  logoutUrl: `${process.env.REACT_APP_API_HOST}/splitwise/logout/`,
+  groupApiUrl: `${process.env.REACT_APP_API_HOST}/splitwise/groups_info/`,
+  friendsApiUrl: `${process.env.REACT_APP_API_HOST}/splitwise/friends_info/`,
+  uploadApiUrl: `${process.env.REACT_APP_API_HOST}/splitwise/upload_csv/`,
+  createBulkTransactions: `${process.env.REACT_APP_API_HOST}/splitwise/create_bulk_transactions/`
+};
 
 export default config;
